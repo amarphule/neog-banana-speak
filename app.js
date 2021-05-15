9c935be1b19639b06263abee9142a5da0677283a
Amar Phule <amardeepphule@gmail.com>
2021-05-15 15:19:29 +0530

    used minions api

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ var btntranslate = document.querySelector('#btn-translate')
 var textInput = document.querySelector("#txt-input")
 var textoutput = document.querySelector("#output")
 
-function inputHandler() {
-    textoutput.innerText = textInput.value + " translated"
+// var serverurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverurl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationUrl(text) {
+    return serverurl + "?text=" + text
 }
 
-btntranslate.addEventListener("click", inputHandler)
+function errorhandler(error) {
+    console.log("Error Occured", error)
+}
+
+function clickHandler() {
+    var inputText = textInput.value;
+    fetch(getTranslationUrl(inputText))
+        .then(respnose => respnose.json())
+        .then(json => {
+            var translatedText = json.contents.translated
+            textoutput.innerText = translatedText
+        })
+        .catch(errorhandler)
+}
+
+btntranslate.addEventListener("click", clickHandler)
